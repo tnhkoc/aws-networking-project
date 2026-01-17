e9fe8b033e5f14f9530293d189aa44c5cd9f3c0f
Tunahan Koc <tunahankoc@Tunahans-Mac-mini.local>
2026-01-17 16:30:49 +0300

    chore: terraform fmt

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,18 +10,18 @@ output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
 
-output "alb_sg_id" { 
-    value = module.security_groups.alb_sg_id
+output "alb_sg_id" {
+  value = module.security_groups.alb_sg_id
 }
 
 output "bastion_sg_id" {
-    value = module.security_groups.bastion_sg_id
+  value = module.security_groups.bastion_sg_id
 }
 
 output "app_sg_id" {
-    value = module.security_groups.app_sg_id
+  value = module.security_groups.app_sg_id
 }
 
 output "db_sg_id" {
-    value = module.security_groups.db_sg_id
+  value = module.security_groups.db_sg_id
 }
